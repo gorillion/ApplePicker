2f876cb15ed5f4828f825d92083297c8258431d9
David Farrell <gorillion@users.noreply.github.com>
2018-09-14 09:46:51 -0400

    basic scripting for tree added

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
 
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondBetweenAppleDrops = 1f;
 	// Use this for initialization
 	void Start () {
 		
