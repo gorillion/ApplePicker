bcd82fbf3f4d7930f8ef8c86f721808ac797d575
David Farrell <gorillion@users.noreply.github.com>
2018-09-14 10:01:20 -0400

    random movement + movement movement

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -20,6 +20,22 @@ public class AppleTree : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+            speed = Mathf.Abs(speed);
+        else if (pos.x > leftAndRightEdge)
+            speed = -Mathf.Abs(speed);
+
+
 	}
+
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+            speed *= -1;
+
+    }
 }
